2e35dc8cafa52aa9c0cb40805468eb4ea93681a9
Vanny macOS <vannysmilekh@gmail.com>
2026-07-10 09:55:34 +0700

    feat: initialize application instance and set public path for production environment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -18,3 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+    if (($_SERVER['SERVER_NAME'] ?? '') === 'www.kimmex.com.kh' || ($_SERVER['SERVER_NAME'] ?? '') === 'kimmex.com.kh') {
+        $app->usePublicPath(dirname($app->basePath()).'/public_html');
+    }
+return $app;
